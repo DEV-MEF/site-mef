7a6b7bf6fd3a781cd71712cf27b5e6fc6afc735e
ahmedjorge <ahmedferreira10@hotmail.com>
2025-03-21 12:22:45 +0000

    commit

--- src/settings/axios.ts
+++ src/settings/axios.ts
@@ -7,7 +7,7 @@
 import axios from "axios"
 
 export const AxiosHttpClient = axios.create({
-    baseURL: process.env.BASE_SERVER+"/api"
+    baseURL: process.env.WEB_BASE_SERVER+"/api"
 });
 
 AxiosHttpClient.interceptors.request.use(
