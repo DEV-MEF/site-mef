0d6dfbc2ba6738af2ec3c6930bebbd464a87f473
Perdigao-wander <oliveirawander1998@gmail.com>
2025-03-21 10:15:39 +0000

    Carregamento dinamico de Nossa Equipa

--- src/components/pages/ministry/out-team-section/index.tsx
+++ src/components/pages/ministry/out-team-section/index.tsx
@@ -2,21 +2,16 @@
 
 import {useEffect, useState} from "react";
 import {AxiosHttpClient} from "@/settings/axios";
-
-interface TeamSection {
-    title: string;
-    members: string[];
-}
+import ContentRenderer from "@/lib/utils";
 
 export default function OurteamSection() {
-    const [teamSections, setTeamSections] = useState<TeamSection[]>([]);
+    const [teamSections, setTeamSections] = useState([]);
 
     useEffect(() => {
         AxiosHttpClient.get("/teams")
-            .then((response) => {
-                if (response?.data) {
-                    setTeamSections(response.data[0].content);
-                    console.log(response.data[0].content)
+            .then(({data: {data}}) => {
+                if (data) {
+                    setTeamSections(data);
                 }
             })
             .catch((error) => {
@@ -24,35 +19,15 @@ export default function OurteamSection() {
             });
     }, []);
 
-
-    const renderData = (data: any[]) => {
-        return data.map(item => {
-            if (item.type === 'paragraph') {
-                return `<p class="text-md text-light mb-6" >${item.children.map(child => {
-                    if (child.bold) {
-                        return `<b>${child.text}</b>`;
-                    }
-                    return child.text;
-                }).join('')}</p>`;
-            }
-
-            if (item.type === 'list') {
-                const listItems = item.children.map(listItem => {
-                    return `<li class="font-light text-justify mb-4 text-sm pl-5">${listItem.children.map(child => child.text).join('')}</li>`;
-                }).join('');
-                return `<ul class="font-light text-justify mb-4 text-sm pl-5">${listItems}</ul>`;
-            }
-
-            return '';
-        }).join('');
-    };
-
-
     return (
         <section className="w-full">
-            <div dangerouslySetInnerHTML={{ __html: renderData(teamSections) }} />
+
+            {
+                teamSections.map(({content, id}) => {
+                    return <ContentRenderer key={id} content={content} type={"blocks"} />
+                })
+            }
+
         </section>
-
     );
-
 }
