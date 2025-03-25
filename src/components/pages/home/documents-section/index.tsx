331e93fbf3f8967fbda7abb20d3a47376b078c92
watsonsantos <watsontavares72@gmail.com>
2025-03-25 16:14:53 +0000

    chore: update document-section

--- src/components/pages/home/documents-section/index.tsx
+++ src/components/pages/home/documents-section/index.tsx
@@ -1,48 +1,51 @@
 import { FileText, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import Title from "@/components/layout/title";
-import {useEffect, useState} from "react";
-import {usePdfViewer} from "@/components/contexts/pdf-viewer";
-import {AxiosHttpClient} from "@/settings/axios";
-import {PdfViewer} from "@/lib/pdf-viewer";
+import { useEffect, useState } from "react";
+import { usePdfViewer } from "@/components/contexts/pdf-viewer";
+import { AxiosHttpClient } from "@/settings/axios";
+import { PdfViewer } from "@/lib/pdf-viewer";
 import moment from "moment";
 import qs from "qs";
 
 type Doc = {
-  documentId: string
-  name: string,
-  content: string,
-  summary: string,
-  files: File
-  publishedAt: string
-}
+  documentId: string;
+  name: string;
+  content: string;
+  summary: string;
+  files: File;
+  publishedAt: string;
+};
 
 type File = {
-  url: string,
-  name: string
-}
+  url: string;
+  name: string;
+};
 
 export default function DocumentsSection() {
   const [files, setFiles] = useState<Doc[]>([]);
-  const {openNewDocument} = usePdfViewer();
+  const { openNewDocument } = usePdfViewer();
 
   useEffect(() => {
-    const query = qs.stringify({
-      sort: ['createdAt:desc'],
-      pagination: {
-        start: 0,
-        limit: 4
+    const query = qs.stringify(
+      {
+        sort: ["createdAt:desc"],
+        pagination: {
+          start: 0,
+          limit: 4,
+        },
+        populate: "*",
       },
-      populate: "*"
-    }, {
-      encodeValuesOnly: true,
-    });
+      {
+        encodeValuesOnly: true,
+      }
+    );
 
     (async () => {
-      AxiosHttpClient.get(`/docs?${query}`).then(({data : {data}}) => {
+      AxiosHttpClient.get(`/docs?${query}`).then(({ data: { data } }) => {
         setFiles(data);
       });
-    })()
+    })();
   }, []);
 
   return (
@@ -60,12 +63,17 @@ export default function DocumentsSection() {
         </div>
 
         <div className="grid">
-          {
-            files.map((value, index) => <div key={index} className="group">
-              <div className="block p-4 rounded-sm hover:bg-muted/50 transition-colors"
-                   onClick={() => {
-                     openNewDocument({name: value.name, uri: value.files.url, id: value.documentId})
-                   }}
+          {files.map((value, index) => (
+            <div key={index} className="group">
+              <div
+                className="block p-4 rounded-sm hover:bg-muted/50 transition-colors cursor-pointer"
+                onClick={() => {
+                  openNewDocument({
+                    name: value.name,
+                    uri: value.files.url,
+                    id: value.documentId,
+                  });
+                }}
               >
                 <div className="flex items-start gap-3">
                   <FileText className="h-5 w-5 mt-0.5 flex-shrink-0 text-secondary-blue" />
@@ -74,7 +82,9 @@ export default function DocumentsSection() {
                       <h3 className="font-medium group-hover:text-primary transition-colors text-text-primary hover:underline">
                         {value.name}
                       </h3>
-                      <span className="text-xs text-stone-500">{moment(value.publishedAt).format('DD/MM/YYYY')}</span>
+                      <span className="text-xs text-stone-500">
+                        {moment(value.publishedAt).format("DD/MM/YYYY")}
+                      </span>
                     </div>
                     <div className="text-sm  mt-1 line-clamp-1 text-text-second">
                       {value.summary}
@@ -82,8 +92,8 @@ export default function DocumentsSection() {
                   </div>
                 </div>
               </div>
-            </div>)
-          }
+            </div>
+          ))}
 
           {/*<div className="group">
             <Link
