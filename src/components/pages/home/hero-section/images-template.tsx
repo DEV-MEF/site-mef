b4ec387a2f5a7efc2b6b369fa7d3b16b7cd8d742
watsonsantos <watsontavares72@gmail.com>
2025-03-25 16:12:27 +0000

    chore: git update hero section carousel

--- src/components/pages/home/hero-section/images-template.tsx
+++ src/components/pages/home/hero-section/images-template.tsx
@@ -23,12 +23,13 @@ export default function ImagesTemplate({
         src={src}
         alt={title}
         fill
+        unoptimized
         className="w-full h-full object-cover"
       />
       {/* Gradient Overlay */}
       <div className="absolute inset-0 gradient"></div>
       {/* Text Content */}
-      <div className="w-full container absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bottom-14 md:bottom-16 lg:bottom-8 text-white z-10  px-4 lg:px-8  py-8 md:py-16 lg:py-20">
+      <div className="w-full container absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bottom-14 md:bottom-16 lg:bottom-8 text-white z-10  px-4 lg:px-8  py-8 md:py-16 lg:py-20 flex flex-col items-start justify-start">
         <h2 className="text-3xl md:text-[4xl] md:text-[37px] lg:text-[44px] font-semibold mb-2 md:mb-4 lg:leading-tight">
           {title}
         </h2>
@@ -37,7 +38,7 @@ export default function ImagesTemplate({
         </p>
         <button
             onClick={() => { router.push(link) }}
-            className="bg-transparent text-white border border-white py-2 px-4 rounded hover:bg-primary-blue hover:border-primary-blue transition">
+            className="bg-transparent text-white border font-semibold border-white cursor-pointer py-3 px-12 rounded hover:bg-white hover:text-primary-blue hover:border-primary-blue transition">
           Ler Mais
         </button>
       </div>
