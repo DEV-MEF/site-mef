41f2099c9b6dbde0bf85d6c48f8161a34acec735
watsonsantos <watsontavares72@gmail.com>
2025-05-20 15:34:29 +0100

    chore: update videos page

--- src/app/publicacoes/videos/page.tsx
+++ src/app/publicacoes/videos/page.tsx
@@ -1,30 +1,12 @@
-import Image from "next/image";
-import fundo from "@/assets/fundoPages.png";
 import "primeicons/primeicons.css";
 import Multimedia from "./pages/Multimedia";
+import Banner from "@/components/pages/banner";
 
 export default function ministerio() {
   return (
     <div className="mb-20">
-      <div className="relative w-full h-[300px]">
-        <Image
-          src={fundo}
-          alt="Fundo Sobre Nós"
-          fill
-          className="object-cover"
-          quality={100}
-        />
-        <div
-          className="absolute inset-0 bg-[#3A3B7B] opacity-50"
-          aria-hidden="true"
-        ></div>
-        <div className="absolute inset-0 flex items-center px-44 py-56">
-          <h1 className="text-white text-3xl font-bold">
-            Publicações <small className="font-light"> » Vídeos</small>
-          </h1>
-        </div>
-      </div>
-      <div className="container mx-auto py-20">
+      <Banner text_1="Publicações" text_2="Vídeos" link_1="/publicacoes" />
+      <div className="w-full px-4 max-w-[88rem] container mx-auto py-10 mb-20">
         <Multimedia />
       </div>
     </div>
