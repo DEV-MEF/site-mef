1d2ddc0959b6bae2cf8444e8db52b4841de01afb
watsonsantos <watsontavares72@gmail.com>
2025-07-08 12:23:50 +0100

    chore: update skeleton image-video gallery

--- src/components/layout/skeleton/image-video-gallery.tsx
+++ src/components/layout/skeleton/image-video-gallery.tsx
@@ -4,7 +4,7 @@ export function Gallerykeleton() {
   return (
     <section className="w-full max-w-[88rem] mx-auto container px-4 py-12">
       {/* Imagem principal com botões laterais */}
-      <div className="relative w-[60%] aspect-video rounded-xl overflow-hidden bg-muted mt-12 mx-auto">
+      <div className="relative w-full md:w-[60%] aspect-video rounded-xl overflow-hidden bg-muted mt-12 mx-auto">
         <Skeleton className="absolute inset-0 w-full h-[85%] rounded-xl" />
 
         {/* Botões de navegação */}
@@ -17,9 +17,9 @@ export function Gallerykeleton() {
       </div>
 
       {/* Miniaturas */}
-      <div className="flex gap-4 overflow-x-auto -mt-5 w-[60%] mx-auto">
+      <div className="flex gap-4 overflow-x-hidden -mt-5 w-full md:w-[60%] mx-auto">
         {Array.from({ length: 5 }).map((_, i) => (
-          <Skeleton key={i} className="h-20 w-32 flex-shrink-0 rounded-md" />
+          <Skeleton key={i} className="h-16 md:h-20 w-24 md:w-32 flex-shrink-0 rounded-md" />
         ))}
       </div>
     </section>
