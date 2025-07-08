19c9b8904f36a076efc755a128df6e8ce9693b79
watsonsantos <watsontavares72@gmail.com>
2025-07-08 17:43:11 +0100

    chore: update news section; add: create home-news skeleton component

---
+++ src/components/layout/skeleton/home-news.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function HomeNewsSkeleton() {
+  return (
+    <section className="w-full max-w-[88rem] container space-y-6">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <div key={index} className="flex flex-col justify-between h-full">
+            <Skeleton className="h-48 w-full rounded-t-md" />
+            <div className="p-4 space-y-3">
+              <Skeleton className="h-4 w-1/2" />
+              <Skeleton className="h-5 w-11/12" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-10/12" />
+              <div className="pt-2">
+                <Skeleton className="h-8 w-24 rounded-md" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
--- src/components/pages/home/news-section/index.tsx
+++ src/components/pages/home/news-section/index.tsx
@@ -12,11 +12,15 @@ import { MdDateRange } from "react-icons/md";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { ChevronsRight } from "lucide-react";
+import { HomeNewsSkeleton } from "@/components/layout/skeleton/home-news";
 moment.locale("pt");
 
 export default function NewsSection() {
   const [news, setNews] = useState<NewsItem[]>([]);
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const query = qs.stringify(
@@ -34,6 +38,7 @@ export default function NewsSection() {
     );
 
     (async () => {
+      setLoading(true);
       try {
         const {
           data: { data },
@@ -42,7 +47,11 @@ export default function NewsSection() {
           setNews(data);
         }
       } catch (error) {
-        console.error("Erro ao buscar dados das notícias:", error);
+        setError(
+          error instanceof Error ? error.message : "Erro ao carregar notícias"
+        );
+      } finally {
+        setLoading(false);
       }
     })();
   }, []);
@@ -52,7 +61,7 @@ export default function NewsSection() {
   };
 
   return (
-    <section className="w-full max-w-[88rem]  container px-4 py-11 md:gap-3 lg:gap-11">
+    <section className="w-full max-w-[88rem] container px-4 py-11 md:gap-3 lg:gap-11">
       <div className="flex justify-between py-5 items-center">
         <div className="flex flex-col mb-6 gap-2">
           <Title text="ÚLTIMAS NOTÍCIAS" />
@@ -63,51 +72,55 @@ export default function NewsSection() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-4">
-        {news.map((item, index) => (
-          <div
-            key={index}
-            className="bg-white shadow-[0_-10px_39px_0_rgba(8,18,109,.1)] rounded overflow-hidden"
-          >
-            <div className="relative">
-              <Image
-                width={300}
-                height={300}
-                unoptimized
-                src={
-                  item.image?.formats?.large?.url
-                    ? `${imageURLServer}${item.image.formats.large.url}`
-                    : item.image?.formats?.medium?.url
-                    ? `${imageURLServer}${item.image.formats.medium.url}`
-                    : item.image?.url
-                    ? `${imageURLServer}${item.image.url}`
-                    : "/images/ministry-logo.png"
-                }
-                alt={item.image?.alternativeText || "Imagem da notícia"}
-                className="w-full h-[200px] md:h-[250px] object-cover"
-              />
-            </div>
-            <div className="px-4 md:px-6 lg:px-7 py-3 flex flex-col gap-2">
-              <div className="flex items-center text-text-light/90 text-xs mb-2">
-                <MdDateRange size={16} className="mb-1 mr-1" />
-                {moment(item.createdAt, moment.ISO_8601).format("LL")}
+      {loading ? (
+        <HomeNewsSkeleton />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-4">
+          {news.map((item, index) => (
+            <div
+              key={index}
+              className="bg-white shadow-[0_-10px_39px_0_rgba(8,18,109,.1)] rounded overflow-hidden"
+            >
+              <div className="relative">
+                <Image
+                  width={300}
+                  height={300}
+                  unoptimized
+                  src={
+                    item.image?.formats?.large?.url
+                      ? `${imageURLServer}${item.image.formats.large.url}`
+                      : item.image?.formats?.medium?.url
+                      ? `${imageURLServer}${item.image.formats.medium.url}`
+                      : item.image?.url
+                      ? `${imageURLServer}${item.image.url}`
+                      : "/images/ministry-logo.png"
+                  }
+                  alt={item.image?.alternativeText || "Imagem da notícia"}
+                  className="w-full h-[200px] md:h-[250px] object-cover"
+                />
               </div>
-              <h3 className="text-text-primary font-bold text-sm mb-1">
-                {item.title.slice(0, 82)}
-              </h3>
-              <p className="text-text-second text-xs font-normal mb-4">
-                {item.summary?.slice(0, 93) || "A Descrição aqui...."}
-              </p>
-              <button
-                onClick={() => readMore(item.documentId)}
-                className="text-sm w-28 my-3 transition-colors ease-in-out duration-300 text-primary-blue cursor-pointer px-4 py-2 border border-primary-blue hover:bg-primary-blue hover:text-white"
-              >
-                Ler Mais
-              </button>
+              <div className="px-4 md:px-6 lg:px-7 py-3 flex flex-col gap-2">
+                <div className="flex items-center text-text-light/90 text-xs mb-2">
+                  <MdDateRange size={16} className="mb-1 mr-1" />
+                  {moment(item.createdAt, moment.ISO_8601).format("LL")}
+                </div>
+                <h3 className="text-text-primary font-bold text-sm mb-1">
+                  {item.title.slice(0, 82)}
+                </h3>
+                <p className="text-text-second text-xs font-normal mb-4">
+                  {item.summary?.slice(0, 93) || "A Descrição aqui...."}
+                </p>
+                <button
+                  onClick={() => readMore(item.documentId)}
+                  className="text-sm w-28 my-3 transition-colors ease-in-out duration-300 text-primary-blue cursor-pointer px-4 py-2 border border-primary-blue hover:bg-primary-blue hover:text-white"
+                >
+                  Ler Mais
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
       <Link
         href="/publicacoes/noticias"
         className="text-sm hover:underline flex items-center transition-colors text-primary-blue/80 mt-10"
