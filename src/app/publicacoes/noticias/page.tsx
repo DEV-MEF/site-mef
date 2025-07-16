34e365c2cfc8c46d38dbb4306b403ce9a24a6c5d
ahmedjorge <ahmedferreira10@hotmail.com>
2025-07-16 12:36:57 +0000

    melhoria na send mail

--- src/app/publicacoes/noticias/page.tsx
+++ src/app/publicacoes/noticias/page.tsx
@@ -3,13 +3,16 @@
 import "primeicons/primeicons.css";
 import AllNews from "@/components/pages/news/all-news";
 import Banner from "@/components/pages/banner";
+import {Suspense} from "react";
 export default function Noticias() {
   return (
     <main className="w-full mb-20">
       <Banner text_1="Publicações" text_2="Notícias" link_1="#" />
-      <section className="w-full container max-w-[88rem] px-4 mx-auto py-16">
-        <AllNews />
-      </section>
+        <section className="w-full container max-w-[88rem] px-4 mx-auto py-16">
+            <Suspense>
+                <AllNews/>
+            </Suspense>
+        </section>
     </main>
   );
 }
