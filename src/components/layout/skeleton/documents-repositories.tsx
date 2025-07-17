bc6aa4b91f3e37910c0bb98c084e0ac94961f93b
watsonsantos <watsontavares72@gmail.com>
2025-07-17 15:51:05 +0100

    chore: update document repositories and document all files component

--- src/components/layout/skeleton/documents-repositories.tsx
+++ src/components/layout/skeleton/documents-repositories.tsx
@@ -11,7 +11,7 @@ export function RepositoryDocumentsSkeleton() {
       </div>
 
       {/* Lista de documentos */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {Array.from({ length: 8 }).map((_, i) => (
           <Skeleton
             key={i}
--- src/components/pages/publications/documents/all-files.tsx
+++ src/components/pages/publications/documents/all-files.tsx
@@ -4,13 +4,13 @@ import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primeicons/primeicons.css";
 import { AxiosHttpClient } from "@/settings/axios";
-import {Documents, usePdfViewer} from "@/components/contexts/pdf-viewer";
+import { Documents, usePdfViewer } from "@/components/contexts/pdf-viewer";
 // import { useHookFolders } from "@/components/hooks/folders";
 import Banner from "../../banner";
-import { CornerUpLeft } from "lucide-react";
+import { ArrowDownToLine, CornerUpLeft, Eye, FileText } from "lucide-react";
 import { notFound, useRouter } from "next/navigation";
 import { RepositoryDocumentsSkeleton } from "@/components/layout/skeleton/documents-repositories";
-import {useHookFolders} from "@/components/hooks/folders";
+import { useHookFolders } from "@/components/hooks/folders";
 
 type File = {
   url: string;
@@ -29,28 +29,31 @@ if (typeof window !== "undefined" && window.location?.origin) {
 }
 
 const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
-  const {documentId} = React.use(params);
+  const { documentId } = React.use(params);
   const [files, setFiles] = useState<Doc[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const { openNewDocument } = usePdfViewer();
-  const {folders, foldersSelected, setFoldersSelected} = useHookFolders("document", documentId);
+  const { folders, foldersSelected, setFoldersSelected } = useHookFolders(
+    "document",
+    documentId
+  );
   const router = useRouter();
 
-  console.log({foldersSelected})
+  console.log({ foldersSelected });
 
   useEffect(() => {
-      AxiosHttpClient.get(
-        `/docs?filters[folder][documentId][$eq]=${documentId}&populate=*`
-      ).then(({ data: { data } }) => {
-        setLoading(false);
-        if (!data || data.length === 0) {
-          return notFound();
-        }
-        setFiles(data);
-      });
+    AxiosHttpClient.get(
+      `/docs?filters[folder][documentId][$eq]=${documentId}&populate=*`
+    ).then(({ data: { data } }) => {
+      setLoading(false);
+      if (!data || data.length === 0) {
+        return notFound();
+      }
+      setFiles(data);
+    });
   }, [documentId]);
 
-  if ((files.length+folders.length)=== 0 && !loading) {
+  if (files.length + folders.length === 0 && !loading) {
     return (
       <section className="w-full">
         <Banner
@@ -58,21 +61,24 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
           text_2="Documentos"
           link_1="/publicacoes"
           link_2="/publicacoes/documentos"
-          text_3={foldersSelected[foldersSelected.length -1]?.name || documentId}
+          text_3={
+            foldersSelected[foldersSelected.length - 1]?.name || documentId
+          }
         />
         <div className="w-full container max-w-[88rem] mx-auto px-4 py-10">
           <div className="flex justify-between items-center my-12">
             <CornerUpLeft
-                className="text-primary-blue/80 hover:text-primary-blue/90 cursor-pointer"
-                onClick={() => {
-                  foldersSelected.pop();
-                  setFoldersSelected(foldersSelected)
-                  router.back()
-                }}
-                xlinkTitle="Voltar"
+              className="text-primary-blue/80 hover:text-primary-blue/90 cursor-pointer"
+              onClick={() => {
+                foldersSelected.pop();
+                setFoldersSelected(foldersSelected);
+                router.back();
+              }}
+              xlinkTitle="Voltar"
             />
             <p className="text-sm text-[#3b4158a8] flex items-center">
-              <i className="pi pi-inbox mr-2"></i> {(files.length+folders.length)} Resultados
+              <i className="pi pi-inbox mr-2"></i>{" "}
+              {files.length + folders.length} Resultados
             </p>
           </div>
 
@@ -93,7 +99,9 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
           text_2="Documentos"
           link_1="/publicacoes"
           link_2="/publicacoes/documentos"
-          text_3={foldersSelected[foldersSelected.length -1]?.name || documentId}
+          text_3={
+            foldersSelected[foldersSelected.length - 1]?.name || documentId
+          }
         />
         <RepositoryDocumentsSkeleton />
       </section>
@@ -102,16 +110,16 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
 
   const handleDownload = (file: Documents) => {
     if (file?.uri) {
-      console.log({selectedDocument: urlServer + file.uri});
+      console.log({ selectedDocument: urlServer + file.uri });
       const downloadUrl = urlServer + file.uri;
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = downloadUrl;
-      link.download = file.name || 'document.pdf'; // Use document name or a default
+      link.download = file.name || "document.pdf"; // Use document name or a default
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
     }
-  }
+  };
 
   return (
     <section className=" w-full">
@@ -120,7 +128,7 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
         text_2="Documentos"
         link_1="/publicacoes"
         link_2="/publicacoes/documentos"
-        text_3={foldersSelected[foldersSelected.length -1]?.name || documentId}
+        text_3={foldersSelected[foldersSelected.length - 1]?.name || documentId}
       />
       <div className="w-full container px-4 max-w-[88rem] mx-auto py-10">
         {/* Title and Results */}
@@ -130,34 +138,35 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
             className="text-primary-blue/80 hover:text-primary-blue/90 cursor-pointer"
             onClick={() => {
               foldersSelected.pop();
-              setFoldersSelected(foldersSelected)
-              router.back()
+              setFoldersSelected(foldersSelected);
+              router.back();
             }}
             xlinkTitle="Voltar"
           />
           <p className="text-sm text-[#3b4158a8] flex items-center">
-            <i className="pi pi-inbox mr-2"></i> {(files.length+folders.length)} Resultado
-            {(files.length+folders.length) === 1 ? "" : "s"}
+            <i className="pi pi-inbox mr-2"></i> {files.length + folders.length}{" "}
+            Resultado
+            {files.length + folders.length === 1 ? "" : "s"}
           </p>
         </div>
 
         <FolderChildren documentId={documentId} />
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 mt-14">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-14">
           {files.map((doc, index) => (
             <div
               key={index}
-              className="flex flex-col lg:flex-row items-start lg:items-center justify-between bg-white border border-[#E2E8F0] rounded-lg p-4 efects hover:border-[#5151F8]"
+              className="flex flex-col lg:flex-row items-start lg:items-center justify-between bg-white border border-[#E2E8F0] rounded-lg p-[14px] efects hover:border-[#5151F8]"
             >
               {/* Ícone do documento */}
               <div className="flex items-center space-x-4">
-                <div className="flex items-center justify-center w-12 h-12 rounded-lg bg-[#F8FAFC]">
-                  <i className="pi pi-file text-[#5151F8] text-2xl"></i>
+                <div className="flex items-center justify-center w-auto h-auto rounded-lg bg-[#F8FAFC] p-2">
+                  <FileText className=" text-[#5151F8] size-5" />
                 </div>
                 {/* Títulos */}
                 <div>
                   <p
-                    className="text-[#1E293B]/90 font-semibold line-clamp-1"
+                    className="text-[#1E293B]/90 font-semibold line-clamp-1 text-sm md:text-base"
                     title={doc.name}
                   >
                     {doc.name}
@@ -179,7 +188,7 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
                     });
                   }}
                 >
-                  <i className="pi pi-eye"></i>
+                  <Eye size={17} />
                 </button>
                 {/*<button
                   className="cursor-pointer flex items-center justify-center w-8 h-8 border border-[#E2E8F0] hover:border-[#5151F8] rounded-full text-[#64748B] hover:text-[#5151F8]"
@@ -188,16 +197,16 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
                   <i className="pi pi-info-circle"></i>
                 </button>*/}
                 <button
-                  className="cursor-pointer flex items-center justify-center w-8 h-8 border border-[#E2E8F0] hover:border-[#5151F8] rounded-full text-[#64748B] hover:text-[#5151F8]"
-                  title="Download"
+                  className="cursor-pointer flex items-center justify-center w-8 h-8  border border-[#E2E8F0] hover:border-[#5151F8] rounded-full text-[#64748B] hover:text-[#5151F8]"
+                  title="Descarregar"
                   onClick={() => {
                     handleDownload({
                       uri: doc.files.url,
                       name: doc.name,
-                    })
+                    });
                   }}
                 >
-                  <i className="pi pi-download"></i>
+                  <ArrowDownToLine size={16} />
                 </button>
               </div>
             </div>
@@ -208,40 +217,47 @@ const AllFiles = ({ params }: { params: Promise<{ documentId: string }> }) => {
   );
 };
 
+const FolderChildren = ({ documentId }: { documentId: string }) => {
+  const { onClickFolder, folders, listCountByDocumentId } = useHookFolders(
+    "document",
+    documentId
+  );
 
-const FolderChildren = ({documentId}: {documentId: string}) => {
-  const {onClickFolder, folders, listCountByDocumentId} = useHookFolders("document", documentId);
-
-  {/* Grid of Folders */}
-  return folders.length > 0 &&
+  {
+    /* Grid of Folders */
+  }
+  return (
+    folders.length > 0 && (
       <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {folders.map((folder, index) => {
           const count = listCountByDocumentId[folder.documentId];
           return (
+            <div
+              key={index}
+              onClick={() => {
+                onClickFolder(folder);
+              }}
+              className="p-4 border border-[#D6DDEB] rounded-lg text-center flex flex-col items-center cursor-pointer efects hover:border-[#5151F8]"
+            >
+              <i
+                className="pi pi-folder text-3xl text-[#5151F8] mb-3"
+                style={{ display: "block" }}
+              ></i>
+              <p className="text-[#3B4158] text-sm font-semibold mb-2">
+                {folder.name}
+              </p>
               <div
-                  key={index}
-                  onClick={() => {
-                    onClickFolder(folder);
-                  }}
-                  className="p-4 border border-[#D6DDEB] rounded-lg text-center flex flex-col items-center cursor-pointer efects hover:border-[#5151F8]"
+                className="py-1 text-[#5151F8] bg-[#F8F8FD] rounded text-xs px-3"
+                style={{ fontSize: "12px" }}
               >
-                <i
-                    className="pi pi-folder text-3xl text-[#5151F8] mb-3"
-                    style={{display: "block"}}
-                ></i>
-                <p className="text-[#3B4158] text-sm font-semibold mb-2">
-                  {folder.name}
-                </p>
-                <div
-                    className="py-1 text-[#5151F8] bg-[#F8F8FD] rounded text-xs px-3"
-                    style={{fontSize: "12px"}}
-                >
-                  {count} Ite{count === 1 ? "m" : "ns"}
-                </div>
+                {count} Ite{count === 1 ? "m" : "ns"}
               </div>
+            </div>
           );
         })}
       </div>
-}
+    )
+  );
+};
 
 export default AllFiles;
